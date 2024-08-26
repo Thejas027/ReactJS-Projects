bcabedeeb50e4ed9fa3334163d697f924dde5065
Thejas <teju270934@gmail.com>
2024-08-26 23:51:43 +0530

    nealy styled the website using tailwind, learnt how to use tailwind css for react and even have an idea to do it for vanialla also

--- fast-react-pizza/src/features/order/SearchOrders.jsx
+++ fast-react-pizza/src/features/order/SearchOrders.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 
 function SearchOrders() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
   function handleSubmit(e) {
@@ -10,7 +10,7 @@ function SearchOrders() {
     if (!query) return;
 
     navigate(`/order/${query}`);
-    setQuery("");
+    setQuery('');
   }
 
   return (
@@ -19,6 +19,7 @@ function SearchOrders() {
         placeholder="Search Order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="foucs:ring-opacity-50 w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 sm:focus:w-72 focus:outline-none focus:ring focus:ring-yellow-500 sm:w-64"
       />
     </form>
   );
