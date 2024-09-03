0b895ec0739ac491743067b0d333561e78c7d80c
Thejas <teju270934@gmail.com>
2024-09-03 23:05:10 +0530

    updated the sotre.js file

--- fast-react-pizza/src/features/menu/MenuItem.jsx
+++ fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,8 +1,32 @@
 /* eslint-disable react/prop-types */
+import { useDispatch, useSelector } from 'react-redux';
 import { formatCurrency } from '../../utils/helpers';
+
 import Button from './../../ui/Button';
+
+import DeleteItem from '../cart/DeleteItem';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
+
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  
+  const isInCart = currentQuantity > 0;
+  
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      unitPrice,
+      quantity: 1,
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -11,7 +35,7 @@ function MenuItem({ pizza }) {
         alt={name}
         className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ' '}`}
       />
-      <div className="flex grow flex-col pt-0.5 ">
+      <div className="flex grow flex-col pt-0.5">
         <p className="font-medium">{name}</p>
         <p className="text-sm capitalize italic text-stone-500">
           {ingredients.join(', ')}
@@ -24,7 +48,14 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small">Add to cart</Button>
+
+          {isInCart && <DeleteItem pizzaId={id} />}
+
+          {!soldOut && !isInCart && (
+            <Button onClick={handleAddToCart} type="small">
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
