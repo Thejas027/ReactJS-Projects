2e3f919ddde52eeea9658f7b79aaa8b880fc677e
Thejas <teju270934@gmail.com>
2024-09-10 11:46:58 +0530

    added the shopping cart funcitonallity to it

--- fast-react-pizza/src/features/menu/MenuItem.jsx
+++ fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -6,16 +6,17 @@ import Button from './../../ui/Button';
 
 import DeleteItem from '../cart/DeleteItem';
 import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
 
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
-  
+
   const currentQuantity = useSelector(getCurrentQuantityById(id));
-  
+
   const isInCart = currentQuantity > 0;
-  
+
   function handleAddToCart() {
     const newItem = {
       pizzaId: id,
@@ -49,7 +50,15 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className='flex items-center gap-3 sm:gap-8'>
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
 
           {!soldOut && !isInCart && (
             <Button onClick={handleAddToCart} type="small">
