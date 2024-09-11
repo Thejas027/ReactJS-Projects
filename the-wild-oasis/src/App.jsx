7a524cc9a8aed75d4e68c08ba8ce0ed7c40f5c1d
Thejas <teju270934@gmail.com>
2024-09-11 23:04:03 +0530

    applied some styles to it learnt how to use styled components how to reuse it, its a diffent method to style the page

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,5 +1,35 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Headings";
+import Row from "./ui/Row";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Row>
+        <Row type="horizontal">
+          <Heading as="h1">The wild Oasis</Heading>
+          <div>
+            <Heading as="h2">Check in and out </Heading>
+            <Button>Check in</Button>
+            <Button variation="secondary" size="small">
+              Check out
+            </Button>
+          </div>
+        </Row>
+
+        <Row>
+          <Heading as="h3">Form</Heading>
+          <form type="vertical">
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </form>
+        </Row>
+      </Row>
+    </>
+  );
 }
 
 export default App;
--- the-wild-oasis/src/styles/GlobalStyles.js
+++ the-wild-oasis/src/styles/GlobalStyles.js
@@ -1,19 +1,6 @@
-/*
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-  rel="stylesheet"
-/>
-<link
-  href="https://fonts.googleapis.com/css2?family=Sono:wght@400;500;600&display=swap"
-  rel="stylesheet"
-/>
-*/
+import { createGlobalStyle } from "styled-components";
 
-/* Colors adapted from https://tailwindcss.com/docs/customizing-colors */
-
-:root {
+const GlobalStyles = createGlobalStyle`:root {
   /* Indigo */
   --color-brand-50: #eef2ff;
   --color-brand-100: #e0e7ff;
@@ -155,8 +142,7 @@ img {
   filter: grayscale(var(--image-grayscale)) opacity(var(--image-opacity));
 }
 
-/*
-FOR DARK MODE
+//    DARK MODE
 
 --color-grey-0: #18212f;
 --color-grey-50: #111827;
@@ -193,4 +179,7 @@ FOR DARK MODE
 
 --image-grayscale: 10%;
 --image-opacity: 90%;
-*/
+
+`;
+
+export default GlobalStyles;
