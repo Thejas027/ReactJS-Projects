3b9c6cc8f73877fe02678e7e9387bb5cb6bd648a
Thejas <teju270934@gmail.com>
2024-09-15 21:41:35 +0530

    image upload funcitonality to cabin added

--- the-wild-oasis/src/services/supabase.js
+++ the-wild-oasis/src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://jrgaxqzfwovmnfuygoqn.supabase.co";
+export const supabaseUrl = "https://jrgaxqzfwovmnfuygoqn.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyZ2F4cXpmd292bW5mdXlnb3FuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYyNTEzNDIsImV4cCI6MjA0MTgyNzM0Mn0.llCS_pU3YnRY9HG7DUXVem63kojvw0vr8lezwEIwCEo";
 const supabase = createClient(supabaseUrl, supabaseKey);
